7cf735754ea5f734a85eea948b3c5039ddc92a5e
shirshak-18 <shirshakdixit18@gmail.com>
2026-01-11 22:47:59 +0530

    reset password functionality added

--- controller/userController.js
+++ controller/userController.js
@@ -252,3 +252,192 @@ module.exports.signIn = async (req, res) => {
     });
   }
 };
+
+module.exports.sendOtpForPasswordReset = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).send({
+        success: false,
+        message: "user does not exist",
+      });
+    }
+    const OTP = otpGenerator.generate(6, {
+      digits: true,
+      lowerCaseAlphabets: false,
+      upperCaseAlphabets: false,
+      specialChars: false,
+    });
+
+    const email = req.body.email;
+    const username = email.split("@")[0].toLowerCase();
+
+    const mailOptions = {
+      from: process.env.AUTH_EMAIL,
+      to: email,
+      subject: "verify your email",
+      html: `<body style="background-color: grey;">
+                <table
+                    align="center"
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    width="550"
+                    bgcolor="white"
+                    style="border: 2px solid black;"
+                >
+            <tbody>
+            <tr>
+                <td align="center">
+                <table
+                    align="center"
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    class="col-550"
+                    width="550"
+                >
+                    <tbody>
+                    <tr>
+                        <td
+                        align="center"
+                        style="background-color: #4cb96b; height: 50px;"
+                        >
+                        <a href="#" style="text-decoration: none;">
+                            <p
+                            style="
+                                color: white;
+                                font-weight: bold;
+                                margin: 0;
+                            "
+                            >
+                            Pairly 💕
+                            </p>
+                        </a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                </td>
+            </tr>
+
+            <tr style="height: 300px;">
+                <td
+                align="center"
+                style="
+                    border: none;
+                    border-bottom: 2px solid #4cb96b;
+                    padding-right: 20px;
+                    padding-left: 20px;
+                "
+                >
+                <p
+                    style="
+                    font-weight: bolder;
+                    font-size: 20px;
+                    letter-spacing: 0.025em;
+                    color: black;
+                    "
+                >
+                    <span style="font-size: 30px;">
+                    Hello ${username}!
+                    </span>
+                    <br /><br />
+                    Your OTP is
+                    <span style="color: red;">${OTP}</span>
+                    <br />
+                    This OTP is valid only for 5 minutes
+                </p>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        </body>`,
+    };
+    const otp = new Otp({ email, otp: OTP });
+
+    const salt = await bcrypt.genSalt(10);
+
+    otp.otp = await bcrypt.hash(otp.otp, salt);
+
+    const rseult = await otp.save();
+    await transporter
+      .sendMail(mailOptions)
+      .then(() => {
+        res.status(200).json({
+          success: true,
+          message: "Email has been sent",
+        });
+      })
+
+      .catch((err) => {
+        console.log(`Error sending email : ${err.message}`);
+        res.status(500).json({
+          success: false,
+          message: "failed to send email",
+        });
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ success: false, message: "Server Error" });
+  }
+};
+
+module.exports.verifyOtpAndResetPassword = async (req, res) => {
+  try {
+    const email = req.body.email.toLowerCase();
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+
+    if (!user) {
+      return res.status(400).send({
+        success: false,
+        message: "user does not exist",
+      });
+    }
+
+    const optHolder = await Otp.find({
+      email: req.body.email,
+    });
+
+    if (optHolder.length === 0) {
+      return res.status(400).json({
+        message: "Otp Expierd or Invalid",
+      });
+    }
+
+    const rightOtpFind = optHolder[optHolder.length - 1];
+    const validUser = await bcrypt.compare(req.body.otp, rightOtpFind.otp);
+    if (!validUser) {
+      return res.status(403).json({
+        message: "Invalid OTP",
+      });
+    }
+    const password = await bcrypt.hash(req.body.password, 10);
+
+    if (rightOtpFind.email === req.body.email && validUser) {
+      user.password = password;
+      const result = await user.save();
+      const OTPDelete = await Otp.deleteMany({
+        email: rightOtpFind.email,
+      });
+
+      return res.status(200).send({
+        success: true,
+        message: "Password Reset successfully",
+      });
+    } else {
+      return res.status(400).send({
+        success: false,
+        message: "Invalid OTP",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,15 @@
 const { Router } = require("express");
-const { signUp, verifyOtp, signIn } = require("../controller/userController");
+const {
+  signUp,
+  verifyOtp,
+  signIn,
+  sendOtpForPasswordReset,
+  verifyOtpAndResetPassword,
+} = require("../controller/userController");
 router = Router();
 router.post("/signup", signUp);
 router.post("/signup/verify", verifyOtp);
 router.post("/signin", signIn);
+router.post("/forgot-password", sendOtpForPasswordReset);
+router.post("/forgot-password/verify", verifyOtpAndResetPassword);
 module.exports = router;
