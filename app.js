8acff0046389e19a0cc32e567c00cff4e4c32070
shirshak-18 <shirshakdixit18@gmail.com>
2026-02-02 18:12:22 +0530

    cors added

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const statusRouter = require("./router/statusRouter");
 const app = express();
 const cors = require("cors");
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  }),
+);
 app.use("/user", userRouter);
 app.use("/status", statusRouter);
 module.exports = app;
