fa2e978ba39a6d308d9b7f780b000c1d5df9ea39
shirshak-18 <shirshakdixit18@gmail.com>
2026-02-08 11:58:09 +0530

    cors preflight dubugging

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://pairly-rho.vercel.app/",
-];
+const cors = require("cors");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://pairly-rho.vercel.app/"],
+    origin: ["http://localhost:5173", "https://pairly-rho.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
